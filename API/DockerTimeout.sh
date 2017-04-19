81dd3637adc747fc72ca3a67b9fa052f92fbcaf4
Sergey <fjdiod@gmail.com>
2017-04-19 15:13:21 +0300

    fix

--- API/DockerTimeout.sh
+++ API/DockerTimeout.sh
@@ -4,7 +4,7 @@ set -e
 to=$1
 shift
 
-cont=$(docker run --rm "$@")
+cont=$(docker run -d "$@")
 code=$(timeout "$to" docker wait "$cont" || true)
 docker kill $cont &> /dev/null
 echo -n 'status: '
