7b52fc080de0239e56145de8d2f99840d6747bd4
Sergey <fjdiod@gmail.com>
2017-04-25 19:51:06 +0300

    fix

--- API/app.js
+++ API/app.js
@@ -68,17 +68,7 @@ app.post('/compile',bruteforce.prevent,function(req, res)
     var vm_name='virtual_machine'; //name of virtual machine that we want to execute
     var timeout_value=20;//Timeout Value, In Seconds
 
-    //details of this are present in DockerSandbox.js
-    var sandboxType = new sandBox(timeout_value,path,folder,vm_name,arr.compilerArray[language][0],arr.compilerArray[language][1],code,arr.compilerArray[language][2],arr.compilerArray[language][3],arr.compilerArray[language][4],stdin);
-
-
-    //data will contain the output of the compiled/interpreted code
-    //the result maybe normal program output, list of error messages or a Timeout error
-    sandboxType.run(function(data,exec_time,err)
-    {
-        //console.log("Data: received: "+ data)
-    	res.send({output:data, langid: language,code:code, errors:err, time:exec_time});
-    });
+    res.send('code');
    
 });
 
